2b89f348c2e333f94b5b90d9100faaf4d68406f5
Diego Gay Saez <diego.gay@gmail.com>
2018-07-25 22:23:34 +0200

    BoarRow and Board components created.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const DIMENSION = 3;
+
+const BoardRow = ({ rowCells, rowIndex }) => (
+  <div>{rowCells.map((_, k) => <button key={`${rowIndex}${k}`} />)}</div>
+);
+const Board = ({ board }) => (
+  <div>
+    {board.map((_, k) => <BoardRow key={k} rowCells={board[k]} rowIndex={k} />)}
+  </div>
+);
 
 class App extends Component {
   render() {
+    const boardRowCells = Array(DIMENSION).fill(null);
+    const board = Array(DIMENSION).fill(boardRowCells);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,6 +27,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <Board board={board} />
+        </div>
       </div>
     );
   }
